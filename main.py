7c14f0cf439625503b4f5a86eede63358dfa8edf
daniel <vdanny911@gmail.com>
2026-09-01 12:55:30 -0500

    FUNCTION reverse_index(data) - This function takes in data from a json file - iterates through every topic's description, make the   description in a set of words, lowercaes, no punctuations,   has characters, is english and adds them to new_dict{}  -The fucntion then interates through every topic's strings in new_dict, checks if the word hasnt been added to the new reversed_dict{}  - Then adds the word and empty list,  -then new loop iterates through every topic's word and adds topic to its list

--- main.py
+++ main.py
@@ -1,3 +1,66 @@
+import json
+import pprint as pp
+import string
+
 """
-this file is where the searching is going to happen
+1. make description -> set of words, but lowercase everything and clean it up
+
 """
+file_name = "topic_dict.json"
+with open(file_name) as json_file:
+    data = json.load(json_file)
+
+
+def is_english(text):
+    # Checks if the string can be cleanly converted to standard ASCII
+    try:
+        text.encode('ascii')
+        return True
+    except UnicodeEncodeError:
+        return False
+
+#pp.pprint(data)
+
+def reverse_index(data):
+
+    new_dict = {}
+
+
+    for topic in data:
+        value = data[topic]
+        clean = value.lower()
+        clean = value.translate(str.maketrans('', '', string.punctuation))
+        description_set = set()
+        for word in clean.split():
+            if word.isalpha() and is_english(word):
+                description_set.add(word)
+        new_dict[topic] = list(description_set)
+
+
+    reversed_dict = {}
+    '''
+    going through every word in the list, for every word that is new, add that as a key into reversed_dict,
+    if topic has that word in it, add topic to a set of topics attached to that word
+    
+    '''
+
+    for topic in new_dict:
+        value = new_dict[topic]
+        for word in value:
+
+            if word not in reversed_dict:
+
+                reversed_dict[word] = []
+
+    for topic in new_dict:
+        value = new_dict[topic]
+        for word in value:
+            reversed_dict[word].append(topic)
+
+    pp.pprint(reversed_dict)
+
+    pp.pprint(reversed_dict["explosion"])
+
+
+
+reverse_index(data)
