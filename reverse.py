eba2179403f5e4c62ca903b642b8c732bcc1eb70
daniel <vdanny911@gmail.com>
2026-09-03 16:10:38 -0500

    Search added: -Very simple way of searching made in search.py!

--- reverse.py
+++ reverse.py
@@ -55,10 +55,9 @@ def reverse_index(data):
 
     #deleting any left over empty value lists.
     for key in list(reversed_dict.keys()):
-        if reversed_dict[key] == []:
+        if not reversed_dict[key]:
             del reversed_dict[key]
 
     return reversed_dict
+#pp.pprint(reverse_index(data))
 
-
-pp.pprint(reverse_index(data))
--- search.py
+++ search.py
@@ -1,3 +1,20 @@
 """
 Where we will make search function to look through the reversed index dictionary we made!
 """
+import json
+from reverse import reverse_index
+import pprint as pp
+
+file_name = "topic_dict.json"
+with open(file_name) as json_file:
+    data = json.load(json_file)
+
+dictionary = reverse_index(data)
+
+
+print("MINECRAFT SEARCH ENGINE")
+input_search = input("What would you like to search?: ")
+
+if input_search in dictionary:
+    print(f"The topics you are looking for: {dictionary[input_search]}")
+
